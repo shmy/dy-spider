fe4139364588b101d258f90e402e8740a6f2fbbe
wangchao <914111374>
2018-06-13 16:15:26 +0800

    updated

--- model/connect.js
+++ model/connect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const os = require('os');
-const address = os.platform() === 'liunx' ? '172.17.0.1' : 'localhost'
+const address = os.platform() === 'linux' ? '172.17.0.1' : 'localhost'
 mongoose.connect(`mongodb://${address}:27017/dy`);
 
 module.exports = mongoose;
