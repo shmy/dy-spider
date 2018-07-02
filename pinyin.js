121a16ef1c0aba1bcddabb4c3d854190286c627a
wangchao <914111374>
2018-07-02 15:15:26 +0800

    updated

--- pinyin.js
+++ pinyin.js
@@ -9,13 +9,19 @@ console.time(1);
     keyword += item.alias.join('');
     keyword += item.director.join('');
     keyword += item.starring.join('');
-    keyword += pinyin(keyword, {
+    let normal = pinyin(keyword, {
       style: pinyin.STYLE_NORMAL, // 设置拼音风格
       heteronym: true
     }).map(i => i[0]).join('');
+    let firstLetter = pinyin(keyword, {
+      style: pinyin.STYLE_FIRST_LETTER, // 设置拼音风格
+      heteronym: true
+    }).map(i => i[0]).join('');
+    keyword += normal;
+    keyword += firstLetter;
     await model.videoModel.update({ _id: item._id }, { keyword })
     i ++;
-    console.log(i, keyword);
+    console.log(i, keyword + normal + firstLetter);
   }
   console.timeEnd(1);
 })();
--- saver.js
+++ saver.js
@@ -1,18 +1,30 @@
 const model = require('./model');
+const pinyin = require('pinyin');
 let count = 0;
 exports.detailSaver = async payload => {
-  // console.log(payload);
-  // return Promise.resolve();
   if (!payload.name) {
     return Promise.resolve();
   }
+  let keyword = payload.name;
+  keyword += payload.alias.join('');
+  keyword += payload.director.join('');
+  keyword += payload.starring.join('');
+  let normal = pinyin(keyword, {
+    style: pinyin.STYLE_NORMAL, // 全屏
+    heteronym: true
+  }).map(i => i[0]).join('');
+  let firstLetter = pinyin(keyword, {
+    style: pinyin.STYLE_FIRST_LETTER, // 首字母
+    heteronym: true
+  }).map(i => i[0]).join('');
+  
+  payload.keyword = keyword + normal + firstLetter;
   const p = await model.videoModel.findOne({ id: payload.id, source: payload.source });
-  count ++;
-  console.log("#", count);
   if (!p) {
-    console.log('👌开始保存---', "{" + payload.name + "}", payload.id);
+    count ++;
+    console.log('👌开始保存---#', count, "{" + payload.keyword + "}", payload.id);
     return model.videoModel.create(payload);
   }
-  console.log( '😯开始更新---', "{" + payload.name + "}", payload.id);
+  // console.log( '😯开始更新---', "{" + payload.name + "}", payload.id);
   return model.videoModel.update({ id: payload.id }, payload);
 };
