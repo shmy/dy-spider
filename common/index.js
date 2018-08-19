353465061c126b5f902289378bd7cfa6d2c71118
wangchao <914111374>
2018-08-19 09:04:19 +0800

    updated

--- common/index.js
+++ common/index.js
@@ -7,13 +7,13 @@ const ky = require('../classify/kyziyuan');
   new Engine(
     [
       ...zd.classify,
-      ...ky.classify,
+      // ...ky.classify,
     ],
     {
       // 最大资源网
       [zd.source]: new Parser(zd.url, zd.source),
       // 酷云资源网
-      [ky.source]: new Parser(ky.url, ky.source),
+      // [ky.source]: new Parser(ky.url, ky.source),
     }
   );
 })();
